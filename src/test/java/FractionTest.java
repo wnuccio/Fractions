085c928960c997828b23a7adea4af8fce658fc17
Walter Nuccio <wnuccio@hotmail.com>
2020-08-21 12:34:33 +0200

    rename coherently all tests

--- src/test/java/FractionTest.java
+++ src/test/java/FractionTest.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,6 +18,11 @@ public class FractionTest {
     @Test
     public void reduction_when_num_and_den_have_common_divisor() {
         assertEquals(Fraction.of(2, 3), Fraction.of(6, 9));
+    }
+
+    @Test
+    void reduction_on_fraction_with_sign() {
+        assertEquals(Fraction.of(-2), Fraction.of(-6, 3));
     }
 
     @Test
@@ -51,39 +55,35 @@ public class FractionTest {
     }
 
     @Test
-    public void return_0_on_0_plus_0() {
+    public void zero_plus_zero_returns_zero() {
         assertEquals(Fraction.of(0), Fraction.of(0).plus(Fraction.of(0)));
     }
 
     @Test
-    public void return_5_on_2_plus_3() {
+    public void int_plus_int_returns_int() {
         assertEquals(Fraction.of(5), Fraction.of(2).plus(Fraction.of(3)));
     }
 
     @Test
-    public void addTwoFractions() {
-        assertEquals(Fraction.of(47,15), Fraction.of(7,3).plus(Fraction.of(4,5)));
+    public void fraction_plus_fraction_returns_fraction() {
+        assertEquals(Fraction.of(47, 15), Fraction.of(7, 3).plus(Fraction.of(4, 5)));
     }
 
     @Test
-    void add_between_two_fractions_returns_an_int() {
+    void fraction_plus_fraction_returns_int() {
         assertEquals(Fraction.of(1), Fraction.of(1, 2).plus(Fraction.of(1, 2)));
         assertEquals(Fraction.of(1), Fraction.of(1, 3).plus(Fraction.of(2, 3)));
         assertEquals(Fraction.of(2), Fraction.of(2, 3).plus(Fraction.of(4, 3)));
     }
 
     @Test
-    void fraction_with_sign_assigns_sign_to_numerator() {
+    void fraction_with_sign_has_signs_on_numerator_only() {
         assertEquals(Fraction.of(-2, 3), Fraction.of(2, -3));
     }
 
     @Test
-    void fraction_with_sign_on_num_and_then_is_actually_positive() {
+    void fraction_with_sign_on_num_and_den_is_actually_positive() {
         assertEquals(Fraction.of(2, 3), Fraction.of(-2, -3));
     }
 
-    @Test
-    void reduction_on_fraction_with_sign() {
-        assertEquals(Fraction.of(-2), Fraction.of(-6, 3));
-    }
 }
